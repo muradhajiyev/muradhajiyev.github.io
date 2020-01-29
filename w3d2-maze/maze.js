7b503f4b548195206a715519d041d936bdfaf86b
Murad Haciyev <murad.hajiyev@certoclav.com>
2020-01-29 14:55:04 -0600

    w3d2 Maze

---
+++ w3d2-maze/maze.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    "use strict";
+
+    let isGameStarted = false;
+
+    $(".boundary").mouseover(function(e){
+        if (isGameStarted)
+            $("div.boundary").addClass("youlose");
+    });
+
+    $("body").mouseover(function(e){
+        e.stopPropagation();
+        if (e.target.id === "maze" || e.target.id === "start" || e.target.id === "end")
+            return false;
+
+        if (isGameStarted){
+            $("div.boundary").addClass("youlose");
+            isGameStarted = false;
+            alert("You lost! Try again by clicking the 'S'");
+        }
+
+    })
+
+    $("#start").click(function(){
+        console.log("Game Started");
+        isGameStarted = true;
+    });
+
+    $("#end").click(function(){
+        if (isGameStarted)
+            alert("You won =)");
+        isGameStarted = false;
+    });
+
+
+});
